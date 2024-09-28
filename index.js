07cefd144a22a61684d7711f6508e1c9c0b26452
dldnfla <lwr0221716@naver.com>
2024-09-28 18:12:35 +0900

    Refactor server setup code

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-
-const app = express();
 const sequelize = require('./db.js');
 const userRoute = require('./src/routes/userRoute');
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-// JSON 요청 본문 파싱 미들웨어
-app.use(express.json());
-
+app.use(express.json()); // express 미들웨어 설정 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', userRoute);
 
-// 서버 시작
+
 app.listen(PORT, async () => {
   try {
+    // 데이터베이스 연결 확인
     await sequelize.authenticate();
     console.log('Database connection established.');
     console.log(`Server running on port ${PORT}`);
