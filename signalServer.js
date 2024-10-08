f98210793bb7ab6d0f24d35974b2f97e69b2e869
dldnfla <lwr0221716@naver.com>
2024-10-08 14:36:02 +0900

    add webRTC example

--- signalServer.js
+++ signalServer.js
@@ -1,66 +1,56 @@
-var os = require('os');
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const fromatMessage = require('./message');
-const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./users');
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static('public'))
 
-const Announcement = '공지';
+io.on('connection', (socket) => {
+  socket.on('join', (roomId) => {
+    const selectedRoom = io.sockets.adapter.rooms.get(roomId)
+    const numberOfClients = selectedRoom ? selectedRoom.size : 0
 
-io.on('connection', socket => {
-    socket.on('joinRoom', ({ username, room }) => {
-        const user = userJoin(socket.id, username, room);
-        socket.join(user.room);
-
-        socket.emit('message', fromatMessage(Announcement, '북적북적에 오신것을 환영합니다!!'));
-        socket.broadcast.to(user.room).emit('message', fromatMessage(Announcement, `${user.username}님이 입장하셨습니다.`));
-        
-        io.to(user.room).emit('roomUsers', {
-            room: user.room,
-            users: getRoomUsers(user.room)
-        });
-    });
-
-    socket.on('chatMessage', (msg) => {
-        const user = getCurrentUser(socket.id);
-        io.to(user.room).emit('message', fromatMessage(user.username, msg));
-    });
-    
-    socket.on('disconnect', () => {
-        const user = userLeave(socket.id);
-        if(user) {
-            io.to(user.room).emit('message', fromatMessage(Announcement, `${user.username}님이 퇴장하셨습니다.`));
-        }
-    });
-});
-
-const PORT = 5001 || process.env.PORT;
-
-server.listen(PORT, () => {
-    console.log(`Server has been running..!`);
-    console.log(`[Server address]\n- http://${getIp()}:${PORT}`);
-    console.log(`- http://127.0.0.1:${PORT}`);
-    console.log(`- http://localhost:${PORT}`);
-});
-
-function getIp() {
-    var ifaces = os.networkInterfaces();
-    var ip = '';
-    for (var dev in ifaces) {
-        var alias = 0;
-        ifaces[dev].forEach(function(details) {
-            if (details.family == 'IPv4' && details.internal === false) {
-                ip = details.address;
-                ++alias;
-            }
-        });
+    if (numberOfClients === 0) {
+      console.log(`Creating room ${roomId} and emitting room_created socket event`)
+      socket.join(roomId)
+      socket.emit('room_created', roomId)
+    } else if (numberOfClients === 1) {
+      console.log(`Joining room ${roomId} and emitting room_joined socket event`)
+      socket.join(roomId)
+      socket.emit('room_joined', roomId)
+    } else {
+      console.log(`Can't join room ${roomId}, emitting full_room socket event`)
+      socket.emit('full_room', roomId)
     }
-    return ip;
-}
+  })
+
+  // These events are emitted to all the sockets connected to the same room except the sender.
+  socket.on('start_call', (roomId) => {
+    console.log(`Broadcasting start_call event to peers in room ${roomId}`)
+    socket.to(roomId).emit('start_call')
+  })
+
+  socket.on('webrtc_offer', (event) => {
+    console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
+    socket.to(event.roomId).emit('webrtc_offer', event.sdp)
+  })
+
+  socket.on('webrtc_answer', (event) => {
+    console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
+    socket.to(event.roomId).emit('webrtc_answer', event.sdp)
+  })
+
+  socket.on('webrtc_ice_candidate', (event) => {
+    console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
+    socket.to(event.roomId).emit('webrtc_ice_candidate', event)
+  })
+})
+
+// START THE SERVER =================================================================
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log(`Express server listening on port ${port}`)
+})
