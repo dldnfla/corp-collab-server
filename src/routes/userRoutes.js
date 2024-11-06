28456b337cad453763583e35f08f7d8db960bf48
dldnfla <lwr0221716@naver.com>
2024-11-06 06:22:41 +0000

    modify search user api and swagger

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,7 @@ const { verifyToken } = require('../middleware/auth');
 router.post('/signup', userController.createUser);
 router.post('/login', userController.authenticateUser);
 router.get('/users', verifyToken, userController.getUserById);
-router.get('/users/search/:searchUser', verifyToken,userController.searchUser);
+router.get('/users/:searchUser', verifyToken,userController.searchUser);
 router.put('/users/:userId',verifyToken, userController.updateUser);
 router.put('/users/:userId/weeklyNote',verifyToken, userController.updateWeeklyNote);
 router.delete('/users/:userId', verifyToken, userController.deleteUser);
